11349e31ee0de21d22f4109e49fa39c53c835365
Saidakbar Boistov <150612406+Boistov@users.noreply.github.com>
2024-05-04 19:14:42 +0400

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,9 +1,7 @@
 numbers = [int(x) for x in input().split()]
 
-count = 1
+a = 1
 for i in range(1, len(numbers)):
     if numbers[i] != numbers[i - 1]:
-        count += 1
-
-# Output
-print(count)
+        a += 1
+print(a)
